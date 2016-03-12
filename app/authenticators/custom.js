102b1964bec02ff4572721cc3bf40d017629cf5f
AlexKVal <alexkval@gmail.com>
2016-03-12 22:59:13 +0200

    use more es6 and ember features

--- app/authenticators/custom.js
+++ app/authenticators/custom.js
@@ -1,37 +1,37 @@
 import Ember from 'ember';
 import BaseAuthenticator from 'ember-simple-auth/authenticators/base';
 
-const { RSVP, isEmpty, run } = Ember;
+const { RSVP: { Promise }, isEmpty, run, $ } = Ember;
 
 export default BaseAuthenticator.extend({
   tokenEndpoint: 'http://localhost:3001/sessions/create',
 
   restore(data) {
-    return new RSVP.Promise((resolve, reject) => {
-      if (!isEmpty(data.token)) {
-        resolve(data);
-      } else {
-        reject();
-      }
-    });
+    if (!isEmpty(data.token)) {
+      return Promise.resolve(data);
+    } else {
+      return Promise.reject();
+    }
   },
 
   authenticate(identification, password) {
-    return new RSVP.Promise((resolve, reject) => {
-      Ember.$.ajax({
+    return new Promise((resolve, reject) => {
+      $.ajax({
         url: this.get('tokenEndpoint'),
         type: 'POST',
         data: JSON.stringify({ username: identification, password }),
         contentType: 'application/json;charset=utf-8',
         dataType: 'json'
       })
-      .then((response) => run(() => resolve({ token: response.id_token })),
-      (xhr) => run(() => reject(xhr.responseJSON || xhr.responseText)));
+      .then(
+        (response) => run(null, resolve, { token: response.id_token }),
+        (xhr) => run(null, reject, xhr.responseJSON || xhr.responseText)
+      );
     });
   },
 
   invalidate() {
     console.log('invalidate...');
-    return RSVP.Promise.resolve();
+    return Promise.resolve();
   }
 });
