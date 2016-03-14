357c39166ab2dbaea83ca6fb700b363d7351624f
AlexKVal <alexkval@gmail.com>
2016-03-14 22:44:24 +0200

    protected route shows "Accounts"

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -3,5 +3,5 @@ import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
 
 export default DS.JSONAPIAdapter.extend(DataAdapterMixin, {
   authorizer: 'authorizer:jwt',
-  namespace: 'api/protected'
+  namespace: 'api'
 });
