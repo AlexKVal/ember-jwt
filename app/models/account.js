5213f8ab70d77287f622668fa6f61b5974b6034a
AlexKVal <alexkval@gmail.com>
2016-03-16 22:10:22 +0200

    add isValidated for Account

--- app/models/account.js
+++ app/models/account.js
@@ -1,6 +1,14 @@
 import DS from 'ember-data';
+import Ember from 'ember';
+
+const { computed, isPresent } = Ember;
 
 export default DS.Model.extend({
   name: DS.attr('string'),
-  camelCasedAttribute: DS.attr('string')
+  camelCasedAttribute: DS.attr('string'),
+
+  isValidated: computed('name', 'camelCasedAttribute', function() {
+    const {name, camelCasedAttribute} = this.getProperties('name', 'camelCasedAttribute');
+    return isPresent(name) && isPresent(camelCasedAttribute);
+  })
 });
